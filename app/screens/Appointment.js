1ad5899d54fb16da908636ca62d2ad1f3e08414b
Aditya Bhattacharya <69953590+AdityaBhattacharya1@users.noreply.github.com>
2023-12-14 10:31:34 +0530

    add no appointments found text

--- app/screens/Appointment.js
+++ app/screens/Appointment.js
@@ -3,6 +3,10 @@ import {
 	FlatList,
 	RefreshControl,
 	ActivityIndicator,
+	Text,
+	View,
+	TouchableOpacity,
+	Dimensions,
 } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import PageHeader from '../shared/PageHeader'
@@ -10,11 +14,14 @@ import globalAPI from '../services/globalAPI'
 import { useUser } from '@clerk/clerk-expo'
 import AppointmentCardItem from '../components/appointments/AppointmentCardItem'
 import Colors from '../shared/Colors'
+import { AntDesign } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Appointment() {
 	const { user } = useUser()
 	const [appointmentList, setAppointmentList] = useState([])
 	const [refreshing, setRefreshing] = useState(true)
+	const navigation = useNavigation()
 
 	const getUserAppointments = () => {
 		globalAPI
@@ -45,12 +52,53 @@ export default function Appointment() {
 					style={{ marginTop: '30%' }}
 				/>
 			) : null}
-			<FlatList
-				data={appointmentList}
-				renderItem={({ item, index }) => (
-					<AppointmentCardItem appointment={item} />
-				)}
-			/>
+			{appointmentList.length > 0 ? (
+				<FlatList
+					data={appointmentList}
+					renderItem={({ item, index }) => (
+						<AppointmentCardItem appointment={item} />
+					)}
+				/>
+			) : (
+				<View
+					style={{
+						marginTop: Dimensions.get('screen').height * 0.1,
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<AntDesign
+						name="infocirlce"
+						size={24}
+						color={Colors.PRIMARY}
+					/>
+					<Text
+						style={{
+							fontSize: 20,
+							marginTop: 10,
+							color: Colors.PRIMARY,
+						}}
+					>
+						No appointments found
+					</Text>
+					<TouchableOpacity
+						style={{
+							padding: 16,
+							backgroundColor: Colors.PRIMARY,
+							borderRadius: 90,
+							alignItems: 'center',
+							width: Dimensions.get('screen').width * 0.8,
+							marginTop: 20,
+						}}
+						onPress={() => navigation.navigate('Home')}
+					>
+						<Text style={{ fontSize: 17, color: Colors.WHITE }}>
+							Book An Appointment
+						</Text>
+					</TouchableOpacity>
+				</View>
+			)}
 		</ScrollView>
 	)
 }
